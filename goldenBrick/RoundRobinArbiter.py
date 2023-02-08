25a668d868b7a93d827cbd97350dd3bf0a6cae13
ganrun xu <ganrunxu@umich.edu>
2023-02-08 17:11:57 -0500

    modify arbiter

--- goldenBrick/RoundRobinArbiter.py
+++ goldenBrick/RoundRobinArbiter.py
@@ -1,44 +1,53 @@
 import time
-# row_priority = 4
+import numpy as np
+# bin_priority = 4
 class RoundRobinArbiter:
     def __init__(self, row_num):
-        self.row_priority = []
+
+        self.bin_priority = []
+        self.granded_bin = None
+        self.granded_bin_valid = None
+
+        self.bin_priority = []
         for i in range(row_num):
-            self.row_priority.append(i)
+            self.bin_priority.append(i)
     
     def request(self, requester_id):
-        granded_prority = len(self.row_priority)
-        granded_row = None
-        next_prority = self.row_priority
-        if requester_id != [0,0,0,0]:
+        granded_prority = len(self.bin_priority)
+        next_prority = self.bin_priority
+        self.granded_bin_valid = 0
+        no_valid_request = [0]*len(self.bin_priority)
+        if requester_id != no_valid_request:
+            self.granded_bin_valid = 1 
             for i in range(len(requester_id)):
                 if requester_id[i]:
-                    if self.row_priority[i] < granded_prority:
-                        granded_prority = self.row_priority[i]
-                        granded_row = i
+                    if self.bin_priority[i] < granded_prority:
+                        granded_prority = self.bin_priority[i]
+                        self.granded_bin = i
                     # print(f"update grand_priority{granded_prority}")
-                    # print(f"update requester_id{requester_id}")
-                    # print(f"update self.row_priority{self.row_priority}")       
-                for j in range(len(self.row_priority)):
-                    next_prority[j] = (self.row_priority[j] + len(self.row_priority) - granded_prority - 1) % len(self.row_priority)
-        self.row_priority =next_prority    
+                    # print(f"update self.bin_priority{self.bin_priority}") 
+            for j in range(len(self.bin_priority)):
+                next_prority[j] = (self.bin_priority[j] + len(self.bin_priority) - granded_prority - 1) % len(self.bin_priority)
+        self.bin_priority =next_prority    
 
-        print(f"row {granded_row} granted access")
-        print(f"New priority {self.row_priority}")
+        if self.granded_bin_valid:
+            print(f"row {self.granded_bin} granted access")
+            print(f"New priority {self.bin_priority}")
+        else:
+            print(f"no bin granded")
             
-        return granded_row
+        return (self.granded_bin, self.granded_bin_valid)
                  
 
-# test the Round Robin Arbiter
-# row_priority = 4
-# arbiter = RoundRobinArbiter(row_priority)
-# print(arbiter.row_priority)
+# # test the Round Robin Arbiter
+# bin_priority = 8
+# arbiter = RoundRobinArbiter(bin_priority)
+# # print(arbiter.bin_priority)
 
-# request0 = [0,0,0,0]
-# request1 = [1,0,1,0]
-# request2 = [0,1,0,0]
-# request3 = [0,1,0,1]
-# print(len(request0))
+# request0 = [0,1,0,0,0,0,0,0]
+# request1 = [1,0,1,0,0,0,0,1]
+# request2 = [0,1,0,0,0,0,0,0]
+# request3 = [0,1,0,1,0,0,0,0]
 # arbiter.request(request0)
 # time.sleep(1)
 # arbiter.request(request1)
@@ -47,5 +56,5 @@ class RoundRobinArbiter:
 # time.sleep(1)
 # arbiter.request(request3)
 # time.sleep(1)
-# arbiter.request([0,0,0,0])
+# arbiter.request([0,0,0,0,0,0,0,0])
 
