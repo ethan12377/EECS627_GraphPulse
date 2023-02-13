3f7472f80f128520eeff0755e341b3b3079996ed
pz2001 <hpzhong@umich.edu>
2023-02-13 14:39:59 -0500

    updated pagerank algorithm with matrix to csr conversion method

--- goldenBrick/pagerank.py
+++ goldenBrick/pagerank.py
@@ -1,9 +1,25 @@
 import numpy as np
 from scipy.sparse import csr_matrix
 
+# Source: wikipedia, chatgpt, some slight modifications by Peter Zhong
+
 # matrix format: adjacency matrix where M_i,j represents the link from 'j' to 'i', such that for all 'j', sum(i, M_i,j) = 1
 #                all edges weighted equally
 
+def matrix_to_csr(matrix, filename):
+    rows, cols = len(matrix), len(matrix[0])
+    indptr = [0]
+    col_indices = []
+    for row in matrix:
+        for col_idx, value in enumerate(row):
+            if value == 1:
+                col_indices.append(col_idx)
+        indptr.append(len(col_indices))
+    with open(filename, 'w') as f:
+        f.write(str(col_indices)[1:-1] + '\n')
+        f.write(str(indptr)[1:-1] + '\n')
+
+# converts regular adjacency matrix to have: 1) equally weighted edges with sum = 1 and 2) no sinks
 def csr_to_matrix(filename):
     # read in col index and row index for csr
     with open(filename, 'r') as f:
@@ -53,6 +69,14 @@ def pagerank(M, num_iterations: int = 100, d: float = 0.85):
     return v
 
 if __name__ == '__main__':
+    adj_matrix = np.array([[0, 0, 1, 0, 0],
+                           [0, 0, 1, 1, 0],
+                           [1, 0, 0, 0, 0],
+                           [0, 1, 1, 0, 0],
+                           [0, 0, 1, 0, 0]])
+    adj_matrix = np.array([[0, 1],
+                           [0, 0]])
+    matrix_to_csr(adj_matrix, 'csr.txt')
     M = csr_to_matrix('csr.txt')
     v = pagerank(M, 100, 0.85)
     print(str(v))
