d7d940d0bd5f7fdfc52c6753b21a4180d472fdef
guanzetong <ztguan@umich.edu>
2023-02-25 16:19:31 -0500

    Commit print ground truth to file version

--- goldenBrick/Xbar_SchedToPE_test.py
+++ goldenBrick/Xbar_SchedToPE_test.py
@@ -6,6 +6,8 @@ from crossbar import Xbar_SchedToPE
 PE_timer = np.zeros(4)
 PE_timer_n = np.zeros(4)
 
+np.random.seed(0)
+
 def OB_input():
     for i in range(4):
         if io_port.IssValid[i] == 1:
@@ -32,7 +34,7 @@ def OB_input():
 
 def PE_input():
     for i in range(4):
-        print('PEDelta[', i, '] = ', np.around(io_port.PEDelta[i], 3), '\tPEIdx[', i, '] = ', io_port.PEIdx[i], '\tPEValid[', i, '] = ', io_port.PEValid[i], '\tPEReady[', i, '] = ', io_port.PEReady[i])
+        print('PEDelta[0]', np.around(io_port.PEDelta[i], 3), ' PEIdx[0]', io_port.PEIdx[i], ' PEValid[0]', io_port.PEValid[i], ' PEReady[0]', io_port.PEReady[i])
     
     for i in range(4):
         if io_port.PEReady[i] == np.int8(0):
@@ -65,11 +67,26 @@ if __name__ == "__main__":
     io_port.PEReady_n = np.ones(4, dtype=np.uint8)
     io_port.PEReady = np.ones(4, dtype=np.uint8)
     
-    for i in range(20):
+    fd = open('Xbar_SchedToPE_ground_truth.txt', 'w')
+    
+    for i in range(4):
+        print(f'IssDelta[{i}] IssIdx[{i}] IssValid[{i}] IssReady[{i}]', end=' ', file=fd)
+    for i in range(4):
+        print(f'PEDelta[{i}] PEIdx[{i}] PEValid[{i}] PEReady[{i}]', end=' ', file=fd)
+    print('\n', file=fd)
+
+    
+    for i in range(200):
         print("[Clock", i, "]")
         OB_input()
         Xbar0.one_clock()
         PE_input()
+        
+        for j in range(4):
+            print(hex(io_port.IssDelta[j].view('H'))[2:].zfill(4), hex(io_port.IssIdx[j])[2:].zfill(2), io_port.IssValid[j], io_port.IssReady[j], end=' ', file=fd)
+        for j in range(4):
+            print(hex(io_port.PEDelta[j].view('H'))[2:].zfill(4), hex(io_port.PEIdx[j])[2:].zfill(2), io_port.PEValid[j], io_port.PEReady[j], end=' ', file=fd)
+        print('\n', file=fd)
         
         update()
         Xbar0.update()
